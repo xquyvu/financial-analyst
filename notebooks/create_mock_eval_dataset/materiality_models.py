dd44337a288d3e418380042264cce361e5dd4aa9
Quy Vu <40758906+xquyvu@users.noreply.github.com>
2025-12-04 11:43:29 +0000

    update materiality agent output

--- notebooks/create_mock_eval_dataset/materiality_models.py
+++ notebooks/create_mock_eval_dataset/materiality_models.py
@@ -4,15 +4,19 @@ from pydantic import BaseModel, Field
 
 # List out all the financial metrics that could be refered to as dependencies by any
 # other metric
-type DEPENDENCIES = Literal["Gross Profit", "Cost of Sales"]
+type FINANCIAL_METRIC = Literal["Gross Profit", "Cost of Sales"]
 
 
 class MaterialChange(BaseModel):
+    metric: FINANCIAL_METRIC = Field(
+        ...,
+        description="The financial metric that has undergone a material change",
+    )
     material_change: str = Field(
         ...,
         description="Description of the material change that should be included in the FA",
     )
-    dependencies: list[DEPENDENCIES] = Field(
+    dependencies: list[FINANCIAL_METRIC] = Field(
         ...,
         description="List of financial metrics that are dependencies for this material change",
     )
